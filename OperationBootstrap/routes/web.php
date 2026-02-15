261012468d6c2c13dd10f84fb68fddf131bfbc6f
jason white <jason.white@mstc.edu>
2026-02-14 21:04:11 -0600

    Added routes for Inertia and React, and barebones views for Categories and CategoryGroups.

--- OperationBootstrap/routes/web.php
+++ OperationBootstrap/routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::redirect('/', '/category-groups');
+
+// Inertia/React pages (UI routes)
+Route::get('/category-groups', fn () => Inertia::render('CategoryGroups/Index'))
+    ->name('pages.category-groups.index');
+
+Route::get('/categories', fn () => Inertia::render('Categories/Index'))
+    ->name('pages.categories.index');
