17c898dcaa56ba3688d8da86544fa1708023ac7c
douglas hoffmann <17567835@mstc.edu>
2026-03-20 19:49:41 -0500

    -Created Branch -Created Placeholder AdminPanel view/controller -Created Placeholder StaffPanel view/controller -Updated navbar to only have home, admin, and staff panels

---
+++ A New Hope/Controllers/AdminPanelController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace A_New_Hope.Controllers
+{
+    public class AdminPanelController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
